2e521fc7965c87219660c345872f7b8072f88e48
inesoriany <ines.khoun@ens.psl.eu>
2025-11-07 17:52:03 +0100

    visu

--- 0_DRF.R
+++ 0_DRF.R
@@ -37,10 +37,14 @@ pacman::p_load(
   rr_table_up <- import(here("data", "rr_table_quanti.xlsx"), sheet = "Upper") 
 
 
+# Import functions
+source(here("0_Functions.R"))  
+
+
 ################################################################################################################################
 #                                                      3. PARAMETERS                                                           #
 ################################################################################################################################
-  
+
 # Number of simulations
 n <- 1000
   
@@ -107,14 +111,110 @@ rr_table_interpolated <- rr_table_long %>%
   mutate(rr_interpolated = if_else(step > max(step[!is.na(simulated_rr)]), NA_real_, rr_interpolated)) %>%
   ungroup() %>% 
   select("simulation_id", "disease", "step", "rr_interpolated")
-  
+
+
+# Random mix
+set.seed(123)
+rr_table_interpolated <- rr_table_interpolated %>%
+  group_by(disease) %>%
+  mutate(simulation_id = match(simulation_id, unique(simulation_id)),
+         simulation_id = sample(unique(simulation_id))[simulation_id]) %>%
+  ungroup()
+
+
+# Mean and IC
+ic95_rr <- rr_table_interpolated %>%
+  group_by(disease, step) %>%
+  summarise(
+    rr_mean = mean(rr_interpolated, na.rm = TRUE),
+    rr_lci = quantile(rr_interpolated, 0.025, na.rm = TRUE), # Lower limit of the 95% CI
+    rr_uci = quantile(rr_interpolated, 0.975, na.rm = TRUE)  # Upper limit of the 95% CI
+  ) %>% 
+  ungroup()
+
+
+
+################################################################################################################################
+#                                                 7. VISUALIZATION OF DRF                                                      #
+################################################################################################################################
+
+# Disease name
+names_disease <- c(
+  "cancer" = "Cancer incidence",
+  "cvd" = "Cardiovascular disease incidence",
+  "dem" = "Dementia",
+  "diab2" = "Type 2 diabetes",
+  "dep" = "Depressive symptoms",
+  "mort" = "All-cause mortality"
+)
+
+# Disease colour
+colors_disease <- c(
+  "cancer" = "firebrick2",      
+  "cvd" = "gold" ,
+  "dem" = "pink" ,
+  "diab2" = "palegreen3",
+  "dep" = "slateblue",
+  "mort" = "steelblue"
+)
+
+
+
+# All-cause mortality
+  # Simulated RR normal distributions
+graph_drf_sim_mort <- graph_sim_DRF("mort")
+plot(graph_drf_sim_mort)
+
+  # RR mean and IC95 points
+graph_drf_mort <- graph_DRF("mort")
+plot(graph_drf_mort)
 
 
 
 
+# Cardiovascular disease incidence
+  # Simulated RR normal distributions
+graph_drf_sim_cvd <- graph_sim_DRF ("cvd")
+plot(graph_drf_sim_cvd)
+
+  # RR mean and IC95 points
+graph_drf_cvd <- graph_DRF ("cvd")
+plot(graph_drf_cvd)
 
 
 
+
+# Cancer incidence
+  # Simulated RR normal distributions
+graph_drf_sim_cancer <- graph_sim_DRF ("cancer")
+plot(graph_drf_sim_cancer)
+
+  # RR mean and IC95 points
+graph_drf_cancer <- graph_DRF ("cancer")
+plot(graph_drf_cancer)
+
+
+
+
+# Type 2 diabetes
+  # Simulated RR normal distributions
+graph_drf_sim_diab2 <- graph_sim_DRF ("diab2")
+plot(graph_drf_sim_diab2)
+
+  # RR mean and IC95 points
+graph_drf_diab2 <- graph_DRF ("diab2")
+plot(graph_drf_diab2)
+
+
+
+# Dementia
+  # Simulated RR normal distributions
+graph_drf_sim_dem <- graph_sim_DRF ("dem")
+plot(graph_drf_sim_dem)
+
+  # RR mean and IC95 points
+graph_drf_dem <- graph_DRF ("dem")
+plot(graph_drf_dem)
 
 
 

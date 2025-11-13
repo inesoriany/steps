cda6379f2246e313420088f28763ecc81d15c4a2
inesoriany <ines.khoun@ens.psl.eu>
2025-11-13 12:30:08 +0100

    step length

---
+++ 0_Parameters.R
@@ -0,0 +1,44 @@
+#################################################
+#################  PARAMETERS ###################
+#################################################
+
+
+## STEP LENGTH (Murtagh et al., 2020)
+step_length_women <- 0.656   # m
+step_length_men <- 0.704     # m
+
+
+## WALKING SPEED (Barban et al, 2022) ----
+walk_speed <- 4.8  # km/h
+
+## DRIVING SPEED (Kahlmeier, Götschi et al, 2017) ----
+paris_car_speed <- 31  # km/h 
+urban_car_speed <-  32
+rural_car_speed <- 60 
+
+
+
+
+##############################################################
+#                       Social cost                          #
+##############################################################
+
+## VALUE OF A STATISTICAL LIFE YEAR FOR 2019 FRANCE ----
+vsl <- 133000
+
+
+
+##############################################################
+#                      CO2 emissions                         #
+##############################################################
+
+# CO2 emissions per km driven
+CO2_emit <- 124                    # 124g CO2 per km
+
+
+
+
+
+
+
+
